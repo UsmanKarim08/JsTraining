212343f0fb00dab3c7f06c1c78c0b7ae4532dda6
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-10-09 16:06:35 +0000

    Variable

--- 01_basic/helloWorld.js
+++ 01_basic/helloWorld.js
@@ -1,1 +1,9 @@
-console.log('hello World')
+const firstName = 'usman';
+var age = 39;
+let email = "some@some.com";
+city= 'union';
+//  firstName= 'Karim'
+age= '28'
+email = 'usman@google.com'
+city ='jersey'
+ console.table([age,email,city])
