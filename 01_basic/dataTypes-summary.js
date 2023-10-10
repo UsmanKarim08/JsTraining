862d4920ea0581197b264c71051b3f50dd5761e4
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-10-10 15:35:03 +0000

    data types

--- 01_basic/dataTypes-summary.js
+++ 01_basic/dataTypes-summary.js
@@ -8,10 +8,32 @@
 // const isLoggedIn = true > Boolean
 // const outSideTemp = null > empty value
 // let userName; > undefined
-
+// const id = Symbol ('usman')
+// const firstName = Symbol ('Karim')
+// console.log(id == firstName)
 //JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they 
 //hold at runtime and can change throughout the program as we assign different values to them
 
 
 // Reference Type ==>> Non- Primitive
 //Arrays,Objects,Functions
+// let testArray = ['Usman','karim',1234];  >> Array
+// console.log(testArray)
+
+// let user = {
+//     'name':'Osman',       >> Object
+//     age : 36
+// }
+// console.log(user)
+
+// let user1 = [
+//     {
+//         'name':'Osman',      
+//     age : 36
+//     },                             >> object of array
+//     {
+//         'name':'Osman',       
+//    age : 36
+//     }
+// ]
+// console.log(user1)
