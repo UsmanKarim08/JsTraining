5b011c2d864b80f0dcbcca21253c5281ebb42485
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-10-10 14:30:39 +0000

    new changes

---
+++ 01_basic/dataTypes-summary.js
@@ -0,0 +1,17 @@
+// difference between primitive and no primitive ==>> call by value and call by reference
+// Primitive ==>> call by value
+//String,Number,Boolean,null,NaN,Symbol,BinInt (some sceintific or large number )
+
+// const name = 'john smith' > String
+// const age = 38 > Number
+// const value = 69.12 > Number
+// const isLoggedIn = true > Boolean
+// const outSideTemp = null > empty value
+// let userName; > undefined
+
+//JavaScript is a dynamically typed language, which means that data types of variables are determined by the value they 
+//hold at runtime and can change throughout the program as we assign different values to them
+
+
+// Reference Type ==>> Non- Primitive
+//Arrays,Objects,Functions
---
+++ 01_basic/objects.js
@@ -0,0 +1,18 @@
+let user=[
+    {
+        'firstName': 'usman',
+    'lastName': 'karim',
+    'Age': 32,
+},
+{
+    'firstName': 'Imran',
+    'lastName': 'Khatana',
+    'Age': 38,
+}
+    
+
+]
+console.log(user);
+
+//For Interview 
+//
