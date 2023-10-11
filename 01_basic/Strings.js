11ba61326fc01f830a4d9965bb6375a8b10b152b
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-10-11 16:00:05 +0000

    new code

---
+++ 01_basic/Strings.js
@@ -0,0 +1,25 @@
+// How to declare string
+// let _name = 'Usman'
+// let _last = 'karim'
+// // console.log(_name +' '+ _last);
+
+// // How to do string inerpolation
+// //${} is used put varaible 
+// console.log(`Hello My First Name is ${_name} and my last name is ${_last}` )
+
+// We can declare string by creating boject e.g
+// const gameName = new String('Usman')
+// console.log(gameName.toUpperCase()) >> to convert string into upper case
+// console.log(gameName.toLocaleLowerCase()) >> to convert into lower case
+// console.log(gameName.trim()) >> to trim the space
+// console.log(gameName.charAt(2)) >> to check which number the letter is
+// console.log(gameName.indexOf('m')) >> to check the index of letter
+// console.log(gameName.slice(0,2))  >> to slice the string  we can pass negative value
+// console.log(gameName.substring(1,3)) >> we can not pass negative value in substring
+
+// let url ="https://usman.com/hitesh%20karim"
+                                                    //>> how to replace
+// console.log(url.replace('%20', '-'))
+
+
+
