e5a6d79fa5117f49ebe0dfba4d80f8f0948fc81b
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-10-12 18:31:39 +0000

    objects2

---
+++ 01_basic/objects2.js
@@ -0,0 +1,18 @@
+// singletone object....by creating constructer 
+// let user = new Object()
+// console.log(user)
+
+// non-singletone object
+// let user1 = {}
+// console.log(user1)
+
+//How to concat objects
+const user = {1:'usman',2:'Karim'}
+const user1 = {3:'imran',4:'Karim'}
+// const user3 = {...user,...user1}
+// console.log(user3)
+
+console.log(Object.keys(user1))
+console.log(Object.values(user1))
+//To check the property exist or not
+console.log(user.hasOwnProperty('Karim'))
