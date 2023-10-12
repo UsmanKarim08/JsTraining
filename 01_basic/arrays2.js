4f34d0d795a1e8178aff352f0ecdb69efed79463
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-10-12 15:35:11 +0000

    objects

---
+++ 01_basic/arrays2.js
@@ -0,0 +1,22 @@
+// push array
+let marvel = ['spriderman','batman','superman']
+let cricket = ['England', 'Pakistan', 'Australia']
+let countries = ['India', 'Africa']
+// marvel.push(cricket)
+// console.log(marvel)
+
+//Concatinating array
+// let con = marvel.concat(cricket)
+// console.log(con)
+
+//Spreading the array work like same as concat
+
+// let allThings = [...cricket,...marvel,...countries]
+// console.log(allThings)
+
+//How to convert to array
+console.log(Array.from('Usman'))
+let num = 12;
+let num2 = 15;
+let num3 = 17
+console.log(Array.of(num,num2,num3))
--- 01_basic/objects.js
+++ 01_basic/objects.js
@@ -1,18 +1,34 @@
-let user=[
-    {
-        'firstName': 'usman',
-    'lastName': 'karim',
-    'Age': 32,
-},
-{
-    'firstName': 'Imran',
-    'lastName': 'Khatana',
-    'Age': 38,
-}
-    
 
-]
-console.log(user);
 
 //For Interview 
-//
+// singletone when create through an object it's singletone means he only one type of object (created with constructer)
+//if declare as literals it's singletone
+//object literals
+let user = {
+    name : 'usman',
+    lastName : "karim",
+    age : 38,
+    lastLoggedIn : ['Monday','tuesday']
+}
+
+    
+
+
+
+//How to access the object
+// console.log(user);
+// console.log(user['name']);
+
+//How to change value
+// user.name = 'Imran'
+// console.log(user['name'])
+user.greeting = function(){
+    console.log('good morning')
+}
+user.greeting2 = function(){
+    console.log(`Aslam O Alekum and good morning ${this.name} `)
+
+}
+console.log(user.greeting2())
+console.log(user.greeting())
+
