77cbd77b0f16d151c98028fc190ad1b0676833f0
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-10-20 14:14:30 +0000

    new code

--- 01_basic/controlFlow.js
+++ 01_basic/controlFlow.js
@@ -4,9 +4,37 @@
 //     console.log('Yes')
 // }
 
-if(userName === 'usman') {
-    const userName = 'usman'
-    console.log(`${'imran'} logged in`)
+// if(userName === 'usman') {
+//     const userName = 'usman'
+//     console.log(`${'imran'} logged in`)
+// }else{
+//     console.log('No')
+// }
+// const outSideTemp = 50
+// if(outSideTemp < 40){
+//     console.log('Yes')
+// }
+// const balance= 2000
+// if (balance > 4000){
+//     let firName = 'Usman'
+//     console.log(`${firName} Your Balance is less then  500`)
+// }else if (balance < 3000){
+//     console.log('balance is lower then 3000')
+// }else{
+//     console.log('Balance is updated')
+// }
+
+// const userLoggedIn = true
+// const userValidEmail = false
+// if(userLoggedIn && userValidEmail){
+//     console.log('Valid')
+// }else{
+//     console.log('Invalid Email')
+// }
+const useBrowser = true
+const interConnectionSecure = false
+if(useBrowser || interConnectionSecure){
+    console.log('Yes you can log in')
 }else{
-    console.log('No')
+    console.log("connection not secure")
 }
