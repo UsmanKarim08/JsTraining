8500a8aeedb792b7a9a59f0ca1359c5b8ad42ff3
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-11-13 23:03:48 +0000

    another change

--- 01_basic/practice.js
+++ 01_basic/practice.js
@@ -38,16 +38,31 @@
 //     console.log(`Value of index is ${index}`);
 //     index = index + 3;
 // }
-const books = [
-    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
-    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
-    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
-    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
-    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
-    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
-    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
-    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
-    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
-  ];
-  let fic = books.filter((bk) =>{return bk.genre==='History' && bk.publish > 1995 && bk.edition > 2000} )
-    console.log(fic)
+// const books = [
+//     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+//     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+//     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+//     { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+//     { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+//     { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+//     { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+//     { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+//     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+//   ];
+//   let fic = books.filter((bk) =>{return bk.genre==='History' && bk.publish > 1995 && bk.edition > 2000} )
+//     console.log(fic)
+// const myArr = ['usman', 'imran', 'rayyan', 12,13]
+// for (let index = 0; index < myArr.length; index++) {
+//   if(myArr[3] === 'rayyan'){
+//     console.log(`rayyan is found `)
+//     break
+//   }
+//   const element = myArr[index];
+//   console.log(element)
+  
+// }
+const coding = ["js", "ruby", "java", "python", "cpp"]
+coding.forEach((item, arr, value) => {
+  console.log(item , arr, value);
+});
+
---
+++ 01_basic/practice2.js
@@ -0,0 +1,28 @@
+// const language = {  
+//     name : 'C',
+//     year : 1972,
+//     paradigm : 'Imperative',
+//     author : 'Dennis Ritchie',
+//     company : 'Bell Labs'
+// };
+// // destructuring
+// // const{author: me} = language;
+// console.table(language);
+// const language = ['c++', 'java', 'python', 'javascript'];
+// for (let i = 0; i < language.length; i++) {
+//     if(language[1] === 'java'){
+//         console.log(`Yes ${language[1]} is here`)
+//         break;
+//     }
+//     console.log(language[i]);
+// }
+// const otherNumber = 123.8966
+
+// console.log(Math.min(4, 3, 6, 8));
+
+ const lang= () => {return {name : 'usman', age : 38} }    // this is called imiplicit arrow function where we don't write the return key word it automatically detect that
+// console.log(lang(this))
+const print = () => {
+    console.log(lang())
+}
+print()
