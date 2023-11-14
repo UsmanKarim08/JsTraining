781069def46dae6cca5182866a5c655322fcb57b
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-11-14 19:51:16 +0000

    new practice code

---
+++ 01_basic/practice3.js
@@ -0,0 +1,45 @@
+// let num = '1256'
+// const stringIntoNumber =+ num;
+// console.log(typeof(num))
+// const myArr = [1,2,3,1,4,5]
+// for (let index = 0; index < myArr.length; index++) {
+//     if(index === 2){
+//         console.log(`${index} is found`)
+//         break;
+//     }else{
+//         const element = myArr[index];
+//         console.log(element)
+
+//         continue;
+//     }
+    
+// }
+const obj = {
+    1 : 'usman',
+    2 : 'karim'
+}
+const obj2 = {
+    3 : 'imran',
+    4 : 'karim'
+}
+// const obj3 = {...obj,...obj2} 
+obj[1] = 'rayyan'
+console.log(obj)
+// console.log(obj3);
+
+
+// switch (obj.name) {
+//     case 'imran':
+//         'imran fround'
+//         break;
+//         case 'usman':
+//             console.log('usman found')
+                
+//                 break;
+
+//     default:
+//         break;
+// }
+
+
+
