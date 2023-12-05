e1b2b485e9785431a93b09875f08a20dba09887c
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2023-12-05 13:13:36 +0000

    opps

--- oop/oop.js
+++ oop/oop.js
@@ -1,9 +1,14 @@
-let user = {
-    name: "John",
-    age: 30,
-    isLoggedIn: true,
-    getUserDetails: function () {
-        console.log(`${this.name} logged in`);
-    }
-};
-console.log(user.getUserDetails())
+function car(seats,tires,color,make,year){
+    this.make = make
+    this.year = year
+    this.color = color
+    this.seats = seats
+    this.tires = tires
+}
+ let honda = new car('Honda',2023,'Black',4,4)
+ let toyota = new car ('Toyota',2023,'Black',4,4)
+    let ford = new car ('Ford',2023,'Black',4,4)
+    console.log(honda)
+    console.log(toyota)
+    console.log(ford)
+ 
