1eb7804f3a5e3f64f18b0d5ada13ea9a45b481b2
UsmanKarim08 <130843897+UsmanKarim08@users.noreply.github.com>
2024-01-16 19:57:30 +0000

    Add loop examples and truthy/falsy check

---
+++ practice/loops.js
@@ -0,0 +1,36 @@
+// let myarr = [1,2,3,4,'usman','imran'];
+// myarr.forEach((element, index) => {
+//     console.log(`the element of array is ${element} and index is ${index}`)
+// });
+// let rev = myarr.findIndex(element => element == 'usman');
+// console.log(rev);
+// for(let i =0; i<myarr.length; i++){
+//     if(myarr[i] = 'usman') {
+//         console.log('usman is found');
+//         break;
+//     }
+//     console.log(myarr[i]);
+// }
+// let obj = {
+//     name: 'usman',
+//     lName: 'karim',
+//     age: 23,
+//     city: 'karachi'
+// }
+// for(let key in obj){
+//     console.log(key);
+//     console.log(obj[key]);
+// }
+// let index = 1;
+// while (index < 100) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 3;
+// }
+//truthy and falsy values
+let arr = [];
+if(arr.length == 0){
+    console.log('true');
+}else{
+    console.log('false');
+}
+
